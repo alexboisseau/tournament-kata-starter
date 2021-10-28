a957a93a68237c9c757eb893b76dad1ffed074bc
Thomas Le Naour <lenaourthomas@gmail.com>
2021-10-28 16:47:08 +0200

    fix(tests): fix tournament tests

--- ts-nest/apps/tournament/src/testing/tournament.spec.ts
+++ ts-nest/apps/tournament/src/testing/tournament.spec.ts
@@ -3,7 +3,11 @@ import { INestApplication } from '@nestjs/common';
 import { startApp } from './test.utils';
 import * as request from 'supertest';
 
-const exampleTournament = {} as TournamentToAdd;
+const exampleValidTournament = {
+  name: 'Unreal',
+} as TournamentToAdd;
+
+const exampleInvalidTournament = {} as TournamentToAdd;
 
 describe('/tournament endpoint', () => {
   let app: INestApplication;
@@ -13,10 +17,19 @@ describe('/tournament endpoint', () => {
   });
 
   describe('[POST] when creating a tournament', () => {
+    it('should return a 404 error', async () => {
+      const { body } = await request(app.getHttpServer())
+        .post('/api/tournaments')
+        .send(exampleInvalidTournament)
+        .expect(400);
+
+      expect(body.message).toBe('Tournament must have a name');
+    });
+
     it('should return the correct id', async () => {
       const { body } = await request(app.getHttpServer())
         .post('/api/tournaments')
-        .send(exampleTournament)
+        .send(exampleValidTournament)
         .expect(201);
 
       expect(body.id).not.toBeUndefined();
@@ -25,14 +38,22 @@ describe('/tournament endpoint', () => {
     it('should have stored the tournament', async () => {
       const { body } = await request(app.getHttpServer())
         .post('/api/tournaments')
-        .send(exampleTournament)
+        .send(exampleValidTournament)
         .expect(201);
 
       const get = await request(app.getHttpServer())
         .get(`/api/tournaments/${body.id}`)
         .expect(200);
 
-      expect(get.body.name).toEqual(exampleTournament.name);
+      expect(get.body.name).toEqual(exampleValidTournament.name);
     });
   });
+
+  // describe('[GET]: when fetching a tournamenet by ID', () => {
+  //   it('should return a 404 error', async (tournamentId) => {
+  //     const get = await request(app.getHttpServer()).get(`/api/tournament/`).expect(200);
+
+  //     expect(get.)
+  //   });
+  // });
 });
