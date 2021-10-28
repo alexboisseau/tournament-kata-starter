ac556736c2706ddb97f26c4da2711febc3b4cbb6
maximelarrieu <maxime.larrieu@ynov.com>
2021-10-28 17:58:21 +0200

    :zap: [IN PROGRESS] issue-3 in progress, error in controller to pass tournament id

--- ts-nest/apps/tournament/src/app/controllers/tournament/tournament.controller.ts
+++ ts-nest/apps/tournament/src/app/controllers/tournament/tournament.controller.ts
@@ -7,7 +7,7 @@ import {
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
-import { Tournament, TournamentToAdd } from '../../api-model';
+import { Tournament, TournamentToAdd, Participant } from '../../api-model';
 import { v4 as uuidv4 } from 'uuid';
 import { TournamentRepositoryService } from '../../repositories/tournament-repository.service';
 
@@ -52,5 +52,30 @@ export class TournamentController {
     }
 
     return tournament;
+  }
+
+  @Post()
+  public createParticipant(@Body() participantToAdd: Participant): {
+    id: string;
+  } {
+    const { name, elo } = participantToAdd;
+
+
+    if (!name || !elo) {
+      throw new HttpException(
+        'Participant must have a name and elo',
+        HttpStatus.BAD_REQUEST
+      );
+    } 
+
+    const participant = {
+      id: uuidv4(),
+      name,
+      elo,
+    };
+
+    this.tournamentRepository.saveParticipant(tournament.id, participant);
+
+    return { id: participant.id };
   }
 }
--- ts-nest/apps/tournament/src/app/repositories/tournament-repository.service.ts
+++ ts-nest/apps/tournament/src/app/repositories/tournament-repository.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Tournament } from '../api-model';
+import { Participant, Tournament } from '../api-model';
 
 @Injectable()
 export class TournamentRepositoryService {
@@ -11,5 +11,12 @@ export class TournamentRepositoryService {
 
   public getTournament(tournamentId: string): Tournament {
     return this.tournaments.get(tournamentId);
+  }
+
+  public saveParticipant(tournamentId: string, participant: Participant): void {
+    //this.tournaments.set(tournamentId, participant);
+    const tournament = this.tournaments.get(tournamentId);
+    tournament.participants.push(participant);
+    this.tournaments.set(tournament.id, tournament);
   }
 }
